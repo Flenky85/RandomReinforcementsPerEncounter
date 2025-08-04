087d09bb28312f3160bcaa9a68b304dfd880bdfd
Lolo <mangaralc@gmail.com>
2025-08-04 11:32:16 +0200

    Controlled dynamic loot drops and forced chest to always be unlocked
    - Replaced static BlueprintLoot content with custom LootEntry list
    - Ensured chests spawn with specified loot items
    - Set AlreadyUnlocked = true to prevent lock interaction
    - Cleaned up redundant logic

--- RandomReinforcementsPerEncounter/chestSpawn.cs
+++ RandomReinforcementsPerEncounter/chestSpawn.cs
@@ -1,9 +1,12 @@
 ﻿using Kingmaker;
 using Kingmaker.Blueprints;
 using Kingmaker.Blueprints.Area;
+using Kingmaker.Blueprints.Items;
 using Kingmaker.Blueprints.Loot;
 using Kingmaker.EntitySystem.Entities;
+using Kingmaker.Items;
 using Kingmaker.View.MapObjects;
+using System.Collections.Generic;
 using System.Reflection;
 using UnityEngine;
 
@@ -35,28 +38,45 @@ namespace RandomReinforcementsPerEncounter
             var destroyField = typeof(InteractionLootPart).GetField("m_DestroyWhenEmpty", BindingFlags.Instance | BindingFlags.NonPublic);
             destroyField?.SetValue(lootPart, true);
 
+            lootPart.AlreadyUnlocked = true;
+
             var gameObject = view.gameObject;
             var interactionLoot = gameObject.GetComponent<InteractionLoot>() ?? gameObject.AddComponent<InteractionLoot>();
 
-            var lootBlueprint = ResourcesLibrary.TryGetBlueprint<BlueprintLoot>("62a9f5357c9e58f4abebb4aa4f291842");
-            if (lootBlueprint == null) return;
+            var dummyLootBlueprint = ResourcesLibrary.TryGetBlueprint<BlueprintLoot>("931f5cd963df3984ba96562ae0b206dd");
+            if (dummyLootBlueprint == null) return;
 
             var lootField = typeof(InteractionLootSettings).GetField("m_LootTables", BindingFlags.NonPublic | BindingFlags.Instance);
             if (lootField == null || interactionLoot.Settings == null) return;
 
-            try
+            lootField.SetValue(interactionLoot.Settings, new[]
             {
-                lootField.SetValue(interactionLoot.Settings, new[]
+                dummyLootBlueprint.ToReference<BlueprintLootReference>()
+            });
+
+            var lootEntries = new List<LootEntry>();
+
+            var longsword = ResourcesLibrary.TryGetBlueprint<BlueprintItem>("03d706655c07d804cb9d5a5583f9aec5");
+            if (longsword != null)
+            {
+                lootEntries.Add(new LootEntry
                 {
-                    lootBlueprint.ToReference<BlueprintLootReference>()
+                    Item = longsword.ToReference<BlueprintItemReference>(),
+                    Count = 1
                 });
             }
-            catch
+
+            var gold = ResourcesLibrary.TryGetBlueprint<BlueprintItem>("f2bc0997c24e573448c6c91d2be88afa");
+            if (gold != null)
             {
-                return;
+                lootEntries.Add(new LootEntry
+                {
+                    Item = gold.ToReference<BlueprintItemReference>(),
+                    Count = 3
+                });
             }
 
-            lootPart.AddItems(lootPart.MapObjectLoot);
+            lootPart.AddItems(lootEntries);
         }
     }
 }
